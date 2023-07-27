b3be9e7d08739a50813f6ad1506aef4d9dd15a3f
Mohsin Khan <105243739+mohsin4k@users.noreply.github.com>
2023-07-28 00:48:13 +0530

    till 50: Added Redis file to save the user

--- src/features/auth/controllers/signup.ts
+++ src/features/auth/controllers/signup.ts
@@ -9,6 +9,10 @@ import { BadRequestError } from '@global/helpers/error-handler';
 import { Helpers } from '@global/helpers/helpers';
 import { UploadApiResponse } from 'cloudinary';
 import { uploads } from '@global/helpers/cloudinary-upload';
+import { IUserDocument } from '@user/interfaces/user.interface';
+import { UserCache } from '@service/redis/user.cache';
+
+const userCache: UserCache = new UserCache();
 
 export class SignUp{
   @joiValidation(signupSchema)
@@ -35,6 +39,10 @@ export class SignUp{
     if (!result?.public_id) {
       throw new BadRequestError('File upload: Error occurred. Try again.');
     }
+    //Add to redis cache
+    const userDataForCache: IUserDocument = SignUp.prototype.userData(authData, userObjectId);
+    userDataForCache.profilePicture = `https://res.cloudinary.com/da1xnru4v/image/upload/v${result.version}/${userObjectId}`;
+    await userCache.saveUserToCache(`${userObjectId}`, uId, userDataForCache);
 
     res.status(HTTP_STATUS.CREATED).json({message: 'User created successfully', authData});
   }
@@ -50,5 +58,42 @@ export class SignUp{
       avatarColor,
       createdAt: new Date()
     } as IAuthDocument;
+  }
+
+  private userData(data: IAuthDocument, userObjectId: ObjectId): IUserDocument {
+    const { _id, username, email, uId, password, avatarColor } = data;
+    return {
+      _id: userObjectId,
+      authId: _id,
+      uId,
+      username: Helpers.firstLetterUppercase(username),
+      email,
+      password,
+      avatarColor,
+      profilePicture: '',
+      blocked: [],
+      blockedBy: [],
+      work: '',
+      location: '',
+      school: '',
+      quote: '',
+      bgImageVersion: '',
+      bgImageId: '',
+      followersCount: 0,
+      followingCount: 0,
+      postsCount: 0,
+      notifications: {
+        messages: true,
+        reactions: true,
+        comments: true,
+        follows: true
+      },
+      social: {
+        facebook: '',
+        instagram: '',
+        twitter: '',
+        youtube: ''
+      }
+    } as unknown as IUserDocument;
   }
 }
--- src/shared/services/redis/redis.connection.ts
+++ src/shared/services/redis/redis.connection.ts
@@ -12,9 +12,9 @@ class RedisConnection extends BaseCache {
   async connect(): Promise<void> {
     try {
       await this.client.connect();
-      const res = await this.client.ping();
-      console.log(res);
-      // log.info(`Redis connection: ${await this.client.ping()}`);
+      // const res = await this.client.ping();
+      // console.log(res);
+      log.info(`Redis connection: ${await this.client.ping()}`);
     } catch (error) {
       log.error(error);
     }
